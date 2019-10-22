96a0e14ec1dfc634483b097dce982df89d422198
Ange1D <47231898+Ange1D@users.noreply.github.com>
2019-10-22 13:57:50 -0500

    Initial commit

---
+++ app/src/main/java/com/Ange1D/viewpager2pagerindicator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.Ange1D.viewpager2pagerindicator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
