42c982a6029815f28a97a35f3ac8af413da0d969
Ange1D <47231898+Ange1D@users.noreply.github.com>
2019-10-22 23:07:53 -0500

    Material View Pager Dots Indicator Unable start activity

--- app/src/main/java/com/Ange1D/viewpager2pagerindicator/MainActivity.kt
+++ app/src/main/java/com/Ange1D/viewpager2pagerindicator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.Ange1D.viewpager2pagerindicator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewPager2.adapter = ViewPagerAdapter()
+        val springDotsIndicator = findViewById<WormDotsIndicator>(R.id.spring_dots_indicator)
+        val viewPager2 = findViewById<ViewPager2>(R.id.viewPager2)
+        val adapter = ViewPagerAdapter()
+        viewPager2.adapter = adapter
+        springDotsIndicator.setViewPager2(viewPager2)
+
+
     }
 }
